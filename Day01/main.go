0a02ad331ac4bdd894972e615f86b8f5ada666d7
ElisendaGascon <elisendagascon@icloud.com>
2023-12-01 19:01:18 +0000

    Day 1 part 1

--- Day01/main.go
+++ Day01/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
 
+func main() {
+	rows := parseInput("input.txt")
+
+	var sum int
+
+	for _, row := range rows {
+		firstNum := strings.IndexFunc(row, unicode.IsDigit)
+		lastNum := strings.LastIndexFunc(row, unicode.IsDigit)
+		v, err := strconv.Atoi(string(row[firstNum]) + string(row[lastNum]))
+		if err != nil {
+			fmt.Print(err)
+		}
+		sum += v
+	}
+	fmt.Print(sum)
+}
+
+func parseInput(inputFile string) []string {
+	input, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Print(err)
+	}
+	inputString := string(input)
+
+	rows := strings.Split(inputString, "\n")
+	return rows
 }
